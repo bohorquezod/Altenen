50886cc90e3a0538a2ad4384a52b2d1adb83e052
martinop <martin13_barca@hotmail.com>
2016-06-28 20:33:06 -0400

    NO NEWS? WHY

--- server.js
+++ server.js
@@ -241,68 +241,7 @@ var getFirstCookie = function( user, pass, callback ) {
         }
     );
 }
-function getCards() {
-    return new Promise(function (resolve, reject) {
-        request(options, function (error, response, body) {
-            var temp = []
-            if (!error && response.statusCode == 200) {
-                $ = cheerio.load(body);
-                var td = $(".unread");
-                for (var i = 0; i < td.length; i++) {
-                    var json = {};
-                    title = td[i].attribs.title;
 
-                    json.card = /[\d]{16}/.exec(title);
-
-                    if (json.card) {
-                        json.card = json.card[0];
-                        title = title.replace(json.card, "");
-                    }
-
-                    json.year = /20[1-2]\d/.exec(title);
-
-                    if (json.year != null) {
-                        json.year = json.year[0];
-                        title = title.replace(json.year, "");
-                        json.year = json.year.replace("20", "");
-                    }
-
-                    json.cvv = /[\d]{3}/.exec(title);
-
-                    if (json.cvv != null) {
-                        json.cvv = json.cvv[0];
-                        title = title.replace(json.cvv, "");
-                    }
-
-                    json.mon = /[\d]{2}/.exec(title);
-
-                    if (json.mon != null) {
-                        json.mon = json.mon[0];
-                        title = title.replace(json.mon, "");
-                    }
-
-                    if (!json.year) {
-                        json.year = /[\d]{2}/.exec(title);
-
-                        if (json.year != null) {
-                            json.year = json.year[0];
-                            title = title.replace(json.year, "");
-                        }
-                    }
-
-                    json.mon = json.mon;
-                    json.year = json.year;
-                    if (json.mon <= 0 || json.mon > 12) {
-                        json.mon = null;
-                    }
-                    if (json.card && json.cvv && json.year && json.mon && i > -1)
-                        temp.push(json);
-                }
-            }
-            resolve(temp)
-        });
-    })
-}
 function getNewsCards() {
     getCards().then(function (newCards) {
         var news = [];
@@ -329,6 +268,7 @@ function getNewsCards() {
           }
         }
         else{
+          io.sockets.emit("renew", { cards: temp, status: "No hay nuevas"});
           console.log("No hay nuevas")
         }
         console.log("-----------------------------")
